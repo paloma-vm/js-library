8ef1d5032873bf66eb48885371c6464a247ae715
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-29 12:05:33 -0700

    changed to v2 of Edamam api

--- recipe-api.js
+++ recipe-api.js
@@ -4,10 +4,9 @@
 
 // BASE FUNCTION
 async function getRecipes(searchInput, APP_ID, APP_KEY) {         
-    // v1
+    // v2
+    const path = `https://api.edamam.com/api/recipes/v2search?q=${searchInput}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3`
     // gives up to 3 recipes
-    const path = `https://api.edamam.com/search?q=${searchInput}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3`
-    
     try {
         const res = await fetch(path)
         const json = await res.json()
