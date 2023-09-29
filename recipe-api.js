bce6544b0e8de0bbde244dab8ecaef045ff7a881
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-29 11:56:43 -0700

    formatted json name/url

--- recipe-api.js
+++ recipe-api.js
@@ -1,12 +1,31 @@
-async function getRecipes(searchInput, APP_ID, APP_KEY, success, error) {
-            
-    // v1
-    // gives up to 15 recipes
-    const path = `https://api.edamam.com/search?q=${searchInput}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=15`
-    
-    
-    const res = await fetch(path)
-    const json = await res.json()
+// Functions
+// async function getRecipesByMeal()
 
-    return json
+
+// BASE FUNCTION
+async function getRecipes(searchInput, APP_ID, APP_KEY) {         
+    // v1
+    // gives up to 3 recipes
+    const path = `https://api.edamam.com/search?q=${searchInput}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3`
+    
+    try {
+        const res = await fetch(path)
+        const json = await res.json()
+        
+        const recipes = json.hits.map(hit => {
+            const recipeInfo = {
+                name: hit.recipe.label,
+                link: hit.recipe.url
+            }
+            return recipeInfo
+        })
+       
+        return recipes
+    }  catch (error) {
+        return error
+    }
+}
+
+export {
+    getRecipes
 }
