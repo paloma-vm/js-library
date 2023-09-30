1fb94f66bdfe438419a21e7f67b1bb616fcd7d85
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-29 23:56:00 -0700

    added limit to recipes

--- recipe-api.js
+++ recipe-api.js
@@ -3,7 +3,7 @@
 
 
 // BASE FUNCTION
-async function getRecipes(searchInput, appId, appKey) {         
+async function getRecipes(searchInput, appId, appKey, limit = 3) {         
     // v2
     const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
 
@@ -11,7 +11,7 @@ async function getRecipes(searchInput, appId, appKey) {
         const res = await fetch(path)
         const json = await res.json()
         
-        const recipes = json.hits.map(hit => {
+        const recipes = json.hits.slice(0, limit).map(hit => { // slice limits the number of recipes returned
             const recipeInfo = {
                 name: hit.recipe.label,
                 link: hit.recipe.url
