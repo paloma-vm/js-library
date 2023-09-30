7605c91266dd7a314478012615ef4d53e632c169
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-30 14:38:38 -0700

    linted to airbnb w/ESLint

--- recipe-api.js
+++ recipe-api.js
@@ -1,59 +1,60 @@
 // Functions
+// BASE FUNCTION
+async function getRecipesByQuery(path, limit = 3) {
+  try {
+    const res = await fetch(path);
+    const json = await res.json();
+
+    // slice limits recipes Edamam default 20
+    const recipes = json.hits.slice(0, limit).map((hit) => {
+      const recipeInfo = {
+        name: hit.recipe.label,
+        link: hit.recipe.url,
+      };
+      return recipeInfo;
+    });
+
+    return recipes;
+  } catch (error) {
+    return error;
+  }
+}
+
+// other functions
 
 async function getRecipes(searchInput, appId, appKey) {
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
-    return getRecipesByQuery(path)
+  const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`;
+  return getRecipesByQuery(path);
 }
 
 async function getRecipesByQty(searchInput, appId, appKey, Qty) {
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
-    const limit = Qty
-    return getRecipesByQuery(path, limit)
+  const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`;
+  const limit = Qty;
+  return getRecipesByQuery(path, limit);
 }
 
-async function getRecipesByMaxTime(searchInput, appId, appKey, maxTime) {         
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&time=${maxTime}`
-    return getRecipesByQuery(path)
+async function getRecipesByMaxTime(searchInput, appId, appKey, maxTime) {
+  const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&time=${maxTime}`;
+  return getRecipesByQuery(path);
 }
 
 async function getRecipesByMealType(searchInput, appId, appKey, mealType) {
-    if (mealType !== 'breakfast' && mealType !== 'lunch' && mealType !== 'dinner' && mealType !== 'snack' && mealType !== 'teatime') {
-        throw new Error('Please enter a valid meal type. Choose from breakfast, lunch, dinner, snack, or teatime.')
-    }
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=${mealType}`
-    return getRecipesByQuery(path)
+  if (mealType !== 'breakfast' && mealType !== 'lunch' && mealType !== 'dinner' && mealType !== 'snack' && mealType !== 'teatime') {
+    throw new Error('Please enter a valid meal type. Choose from breakfast, lunch, dinner, snack, or teatime.');
+  }
+  const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=${mealType}`;
+  return getRecipesByQuery(path);
 }
 
 async function getBreakfastRecipes(searchInput, appId, appKey) {
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=Breakfast`
-    return getRecipesByQuery(path)
-}
-
-
-// BASE FUNCTION
-async function getRecipesByQuery(path, limit = 3) {         
-
-    try {
-        const res = await fetch(path)
-        const json = await res.json()
-        
-        const recipes = json.hits.slice(0, limit).map(hit => { // slice limits the number of recipes returned, default for Edamam is 20
-            const recipeInfo = {
-                name: hit.recipe.label,
-                link: hit.recipe.url
-            }
-            return recipeInfo
-        })
-       
-        return recipes
-    }  catch (error) {
-        return error
-    }
+  const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=Breakfast`;
+  return getRecipesByQuery(path);
 }
 
 export {
-    getRecipes,
-    getRecipesByQty, 
-    getRecipesByMealType,
-    getRecipesByMaxTime
-}
+  getRecipes,
+  getRecipesByQty,
+  getRecipesByMealType,
+  getRecipesByMaxTime,
+  getBreakfastRecipes,
+};
