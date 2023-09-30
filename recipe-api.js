dd0468e60ff32c0846d681a8efe8e4da810081a5
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-09-30 13:01:08 -0700

    added getRecipesByQty

--- recipe-api.js
+++ recipe-api.js
@@ -1,11 +1,34 @@
 // Functions
-// async function getRecipesByMeal()
+
+async function getRecipes(searchInput, appId, appKey) {
+    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
+    return getRecipesByQuery(path)
+}
+
+async function getRecipesByQty(searchInput, appId, appKey, Qty) {
+    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
+    const limit = Qty
+    return getRecipesByQuery(path, limit)
+}
+
+// async function getRecipesByTime(searchInput, appId, appKey, limit = 3) {         
+//     // v2
+//     const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
+// }
+
+// async function getRecipesByMealType(searchInput, appId, appKey, limit = 3, mealType) {
+//     const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=${mealType}`
+//     return getRecipes(path)
+// }
+
+async function getBreakfastRecipes(searchInput, appId, appKey, limit = 3) {
+    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}&mealType=Breakfast`
+    return getRecipesByQuery(path)
+}
 
 
 // BASE FUNCTION
-async function getRecipes(searchInput, appId, appKey, limit = 3) {         
-    // v2
-    const path = `https://api.edamam.com/api/recipes/v2?type=public&q=${searchInput}&app_id=${appId}&app_key=${appKey}`
+async function getRecipesByQuery(path, limit = 3) {         
 
     try {
         const res = await fetch(path)
@@ -26,5 +49,6 @@ async function getRecipes(searchInput, appId, appKey, limit = 3) {
 }
 
 export {
-    getRecipes
+    getRecipes,
+    getRecipesByQty
 }
